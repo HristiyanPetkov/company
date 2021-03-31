5a4c07676ff6f645021dc155624074d058a770a6
Hrisitqn Petkov <71525781+HristiyanPetkov@users.noreply.github.com>
2021-03-31 13:34:29 +0300

    Add files via upload

---
+++ Company.py
@@ -0,0 +1,38 @@
+import uuid
+
+class Employee():
+    def __init__(self, name, email):
+         self.name = name
+         self.email = email
+         self.generate_id()
+    
+    def generate_id(self):
+        self.id = uuid.uuid4()
+        print("Employee id: " + str(self.id))
+
+    def print_atributes(self):
+        print("Name: " + self.name)
+        print("Email: " + self.email)
+
+class Company():
+    def __init__(self, name):
+        self.name = name
+        self.employees = []
+
+    def add_employee(self):
+        name = input("Enter name: ")
+        email = input("Enter email: ")
+        self.employees.append(Employee(name, email))
+    
+    def print_employees(self):
+        for i in self.employees:
+            i.print_atributes()
+
+petur = Employee("Petur", "petur@gmail.com")
+petur.print_atributes()
+
+print()
+
+company = Company("PetroVil")
+company.add_employee()
+company.print_employees()
